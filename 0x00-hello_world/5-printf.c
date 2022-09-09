843423ed3b9c2e1c37c1c10a428d1b3021924e0b
paxxchal <59150676+paxxchal@users.noreply.github.com>
2022-09-09 14:29:40 -0700

    betty fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - Prints "with proper grammar, but the outcome is a piece of art,\n"
- *
- *   * Return: Always 0 (success)
- *
- *    */
-
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
+ * Return: Always 0
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
