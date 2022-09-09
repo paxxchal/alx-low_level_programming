f1a226480cad4ca005904a5ab1e1550eaa1f90c6
paxxchal <59150676+paxxchal@users.noreply.github.com>
2022-09-09 13:53:05 -0700

    betty verified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-
 /**
  * main: Write 'Programming is like building a multilingual puzzle'
- * return: Always 0 (success)  
+ * return: Always 0 (success)
  */
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
-		return (0);
-
+	return (0);
 }
