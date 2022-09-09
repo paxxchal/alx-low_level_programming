1fcbd0ab61adc0bba9739cf7491c38f00b7bcb43
paxxchal <59150676+paxxchal@users.noreply.github.com>
2022-09-09 14:13:25 -0700

    betty fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main: Write 'Programming is like building a multilingual puzzle'
- * return: Always 0 (success)
+ * main - Write 'Programming is like building a multilingual puzzle'
+ * Return: Always 0
  */
 int main(void)
 {
