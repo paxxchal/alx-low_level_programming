1b94f03e6c6311fd8f9c5725f0fde81685fcccc9
paxxchal <paschalgabrielacj8@gmail.com>
2022-09-29 00:57:52 +0100

    test

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,15 +6,15 @@
  * return: no return
  */
 
-void _puts_recursion (char *s)
+void _puts_recursion(char *s)
 {
-    if (*s == '\0')
+    if(*s == '\0')
     {
-        _putchar ('\n');
+        _putchar('\n');
     }
     else
     {
-        _putchar (*s);
-        _puts_recursion (s + 1);
+        _putchar(*s);
+        _puts_recursion(s + 1);
     }
 }
