0562c22d68a52b9b969c101cc1cfd60f42abf5eb
paxxchal <paschalgabrielacj8@gmail.com>
2023-02-02 23:39:33 +0100

    Did first task

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_dlistint - doubly linked list node structure
+ * @h: pointer to head of list
+ *
+ * description: doubly linked list node structure
+ * for Holberton project
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t *current = h;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
