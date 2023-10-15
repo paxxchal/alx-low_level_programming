fa684cf28dd63bac64efb05bd9d321eba52c2863
paxxchal <paschalgabrielacj8@gmail.com>
2023-10-15 13:00:14 +0100

    Fixed betty errors

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -13,10 +13,12 @@
 char *generate_key(char *username)
 {
 	int i;
+
 	char *key = malloc(KEY_LENGTH + 1);
+
 	if (key == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	strcpy(key, username);
@@ -29,8 +31,25 @@ char *generate_key(char *username)
 
 	key[strlen(key)] = '\0';
 
-	return key;
+	return (key);
 }
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Description: The `main` function takes a single command-line argument, which
+ * should be a username. It then calls the `generate_key` function to generate
+ * a random key based on the provided username. If the key generation is
+ * successful, it prints the key to the standard output. If memory allocation
+ * fails or the command-line argument count is incorrect, appropriate error
+ * messages are printed to the standard error. The program returns EXIT_SUCCESS
+ * upon successful execution or EXIT_FAILURE in case of errors.
+ *
+ * Return: EXIT_SUCCESS if the program executes successfully, EXIT_FAILURE if
+ * there are errors.
+ */
 
 int main(int argc, char **argv)
 {
@@ -39,19 +58,19 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: %s username\n", argv[0]);
-		return EXIT_FAILURE;
+		return (EXIT_FAILURE);
 	}
 
 	key = generate_key(argv[1]);
 	if (key == NULL)
 	{
 		fprintf(stderr, "Error: Could not generate key\n");
-		return EXIT_FAILURE;
+		return (EXIT_FAILURE);
 	}
 
 	printf("%s\n", key);
 
 	free(key);
 
-	return EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
 }
