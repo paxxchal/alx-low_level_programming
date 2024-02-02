27c4eb6105a2e1ab41fd8d49c78a18179acccb94
paxxchal <paschalgabrielacj8@gmail.com>
2024-02-02 01:08:28 +0100

    pushed a task

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key from the hash table
+ * @ht: Hash table to search
+ * @key: Key string
+ *
+ * Return: The value associated with the key, or NULL if key not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+
+	return (NULL);
+}
